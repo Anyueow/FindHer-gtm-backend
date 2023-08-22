8d19bb4b0e5183182745884581cf87b2516ddd8c
Sanu Das <sanukr1202@gmail.com>
2023-08-23 02:12:24 +0530

    API call issue solved!

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,112 +1,132 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Review = require('../models/reviews');
+const Review = require("../models/reviews");
 const authenticateJWT = require("../middleware/auth");
-
+router.use(express.json());
 // POST - Add a new review
-router.post('/protectedRoute/createReview', authenticateJWT, async (req, res) => {
-    const {
+router.post(
+  "/protectedRoute/createReview",
+  authenticateJWT,
+  async (req, res) => {
+    const { companyName, companyOffice, positionTitle, startDate, endDate } =
+      req.body;
+
+    const userId = req.user.id; // Corrected line
+
+    // Check if the user has filled out the required fields
+    if (
+      !companyName ||
+      !companyOffice ||
+      !positionTitle ||
+      !startDate ||
+      !endDate
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Please fill out all required fields." });
+    }
+
+    try {
+      const initialReview = new Review({
         companyName,
         companyOffice,
         positionTitle,
         startDate,
-        endDate
-    } = req.body;
+        endDate,
+        user: userId, // Include user field in the Review object
+      });
 
-    const userId = req.user.id;  // Corrected line
+      const savedReview = await initialReview.save();
 
-    // Check if the user has filled out the required fields
-    if (!companyName ||  !companyOffice || !positionTitle || !startDate || !endDate) {
-        return res.status(400).json({ message: 'Please fill out all required fields.' });
-    }
-
-    try {
-        const initialReview = new Review({
-                                             companyName,
-                                             companyOffice,
-                                             positionTitle,
-                                             startDate,
-                                             endDate,
-                                             user: userId   // Include user field in the Review object
-                                         });
-
-        const savedReview = await initialReview.save();
-
-        if (savedReview) {
-            // Return success message and review ID
-            res.status(201).json({ message: 'Review successfully created.', reviewId: savedReview._id });
-        } else {
-            res.status(500).json({ message: 'Internal server error.' });
-        }
+      if (savedReview) {
+        // Return success message and review ID
+        res
+          .status(201)
+          .json({
+            message: "Review successfully created.",
+            reviewId: savedReview._id,
+          });
+      } else {
+        res.status(500).json({ message: "Internal server error." });
+      }
     } catch (error) {
-        console.error('Error occurred:', error);
-        res.status(500).json({ message: 'Internal server error.' });
+      console.error("Error occurred:", error);
+      res.status(500).json({ message: "Internal server error." });
     }
-});
+  }
+);
 
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 // ...
 
 // Your route handler
-router.post('/updateRatings', authenticateJWT, async (req, res) => {
-    const { ratings, reviewId } = req.body;
-    const user = req.user.id;
+router.post("/updateRatings", authenticateJWT, async (req, res) => {
+  const { ratings, reviewId } = req.body;
+  const user = req.user.id;
 
-    // Validate reviewId
-    if (!mongoose.Types.ObjectId.isValid(reviewId)) {
-        return res.status(400).json({ message: 'Invalid review ID' });
+  // Validate reviewId
+  if (!mongoose.Types.ObjectId.isValid(reviewId)) {
+    return res.status(400).json({ message: "Invalid review ID" });
+  }
+
+  try {
+    const updatedReview = await Review.findOneAndUpdate(
+      { _id: reviewId, user: user },
+      { $set: { ratings: ratings } },
+      { new: true }
+    );
+
+    if (updatedReview) {
+      res
+        .status(200)
+        .json({
+          message: "Ratings updated successfully.",
+          review: updatedReview,
+        });
+    } else {
+      res.status(400).json({ message: "Review not found." });
     }
-
-    try {
-        const updatedReview = await Review.findOneAndUpdate(
-            { _id: reviewId, user: user },
-            { $set: { ratings: ratings } },
-            { new: true }
-        );
-
-        if (updatedReview) {
-            res.status(200).json({ message: 'Ratings updated successfully.', review: updatedReview });
-        } else {
-            res.status(400).json({ message: 'Review not found.' });
-        }
-    } catch (error) {
-        console.error('Error occurred:', error);
-        res.status(500).json({ message: 'Internal server error.' });
-    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
 });
 
-
 // Update the review with the additional fields:
-router.post('/updateReviewDetails', authenticateJWT, async (req, res) => {
-    const {
-        reviewId,
-        goodThings,
-        badThings,
-        amenities,
-        benefits
-    } = req.body;
+router.post("/updateReviewDetails", authenticateJWT, async (req, res) => {
+  const { reviewId, goodThings, badThings, amenities, benefits } = req.body;
 
-    const user = req.user.id;
+  const user = req.user.id;
 
-    try {
-        const updatedReview = await Review.findOneAndUpdate(
-            { _id: reviewId, user: user },
-            { $set: { goodThings: goodThings, badThings: badThings, amenities: amenities, benefits: benefits } },
-            { new: true }
-        );
+  try {
+    const updatedReview = await Review.findOneAndUpdate(
+      { _id: reviewId, user: user },
+      {
+        $set: {
+          goodThings: goodThings,
+          badThings: badThings,
+          amenities: amenities,
+          benefits: benefits,
+        },
+      },
+      { new: true }
+    );
 
-        if (updatedReview) {
-            res.status(200).json({ message: 'Review details updated successfully.',
-                                     review: updatedReview });
-        } else {
-            res.status(400).json({ message: 'Review not found.' });
-        }
-    } catch (error) {
-        console.error('Error occurred:', error);
-        res.status(500).json({ message: 'Internal server error.' });
+    if (updatedReview) {
+      res
+        .status(200)
+        .json({
+          message: "Review details updated successfully.",
+          review: updatedReview,
+        });
+    } else {
+      res.status(400).json({ message: "Review not found." });
     }
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
 });
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,102 +1,101 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require("../models/user");
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+router.use(express.json());
 
 // POST - Add a new user
-router.post('/register', async (req, res) => {
-    const { email, phoneNumber, password } = req.body;
+router.post("/register", async (req, res) => {
+    console.log("holaaa",req.body);
 
-    // Check if the user has filled out everything
-    if (!email || !phoneNumber || !password) {
-        return res.status(400).json({ message: 'Please fill out all fields.' });
+  const { email, phoneNumber, password } = req.body;
+
+  // Check if the user has filled out everything
+  if (!email || !phoneNumber || !password) {
+    return res.status(400).json({ message: "Please fill out all fields." });
+  }
+
+  try {
+    // Check if there is already an existing user with the same email or phone number
+    const existingUserEmail = await User.findOne({ email: email });
+    if (existingUserEmail) {
+      return res.status(400).json({ message: "Email already in use." });
     }
 
-    try {
-        // Check if there is already an existing user with the same email or phone number
-        const existingUserEmail = await User.findOne({ email: email });
-        if (existingUserEmail) {
-            return res.status(400).json({ message: 'Email already in use.' });
-        }
-
-        const existingUserPhone = await User.findOne({ phoneNumber: phoneNumber });
-        if (existingUserPhone) {
-            return res.status(400).json({ message: 'Phone number already in use.' });
-        }
-
-        // Create new user
-        const newUser = new User({
-                                     email,
-                                     phoneNumber,
-                                     password
-                                 });
-
-        const userReg = await newUser.save();
-
-        // Generate JWT token
-        const token = jwt.sign(
-            { id: newUser._id },
-            process.env.JWT_SECRET,
-            { expiresIn: '24h' }
-        );
-
-        if(userReg) {
-            // Return success message
-            res.status(201).json({message: 'User successfully registered.',
-                                     token: token});
-        } else {         console.error('Error occurred:', error);
-            res.status(500).json({ message: 'Internal server error.' })
-        }
-    } catch (error) {
-        console.error('Error occurred:', error);
-        res.status(500).json({ message: 'Internal server error.' });
+    const existingUserPhone = await User.findOne({ phoneNumber: phoneNumber });
+    if (existingUserPhone) {
+      return res.status(400).json({ message: "Phone number already in use." });
     }
 
+    // Create new user
+    const newUser = new User({
+      email,
+      phoneNumber,
+      password,
+    });
 
+    const userReg = await newUser.save();
 
+    // Generate JWT token
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
+    if (userReg) {
+      // Return success message
+      res
+        .status(201)
+        .json({ message: "User successfully registered.", token: token });
+    } else {
+      console.error("Error occurred:", error);
+      res.status(500).json({ message: "Internal server error." });
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
 });
-
-
 
 // POST - Login an existing user
-router.post('/login', async (req, res) => {
-    const { email, phoneNumber, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { email, phoneNumber, password } = req.body;
 
-    // Check if the user has filled out everything
-    if (!((email || phoneNumber) && password)) {
-        return res.status(400).json({ message: 'Please fill out all fields.' });
+  // Check if the user has filled out everything
+  if (!((email || phoneNumber) && password)) {
+    return res.status(400).json({ message: "Please fill out all fields." });
+  }
+
+  try {
+    // Check if there is an existing user with the same email or phone number
+    const existingUser = await User.findOne({
+      $or: [{ email: email }, { phoneNumber: phoneNumber }],
+    });
+
+    // If the user does not exist or the password does not match, send an error response
+    if (
+      !existingUser || !(await bcrypt.compare(password, existingUser.password))
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Invalid email, phone number or password." });
     }
 
-    try {
-        // Check if there is an existing user with the same email or phone number
-        const existingUser = await User.findOne({ $or: [{ email: email }, { phoneNumber: phoneNumber }] });
+    // Generate JWT token
+    const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
 
-        // If the user does not exist or the password does not match, send an error response
-        if (!existingUser || !await bcrypt.compare(password, existingUser.password)) {
-            return res.status(400).json({ message: 'Invalid email, phone number or password.' });
-        }
-
-        // Generate JWT token
-        const token = jwt.sign(
-            { id: existingUser._id },
-            process.env.JWT_SECRET,
-            { expiresIn: '24h' }
-        );
-
-        // Return success message with token
-        res.status(200).json({
-                                 message: 'User successfully logged in.',
-                                 token: token
-                             });
-    } catch (error) {
-        console.error('Error occurred:', error);
-        res.status(500).json({ message: 'Internal server error.' });
-    }
+    // Return success message with token
+    res.status(200).json({
+      message: "User successfully logged in.",
+      token: token,
+    });
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
 });
-
-
 
 module.exports = router;
